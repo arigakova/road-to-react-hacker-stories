41b1c2a1637c18d981b2b985e7dc190f691a9049
Anna Ryzhakova <arigakova@gmail.com>
2023-01-12 19:05:25 -0500

    Embedded JS in JSX

---
+++ src/DisplayList.js
@@ -0,0 +1,10 @@
+const DisplayList = () => { // DisplayList component
+
+    const greeting = "Good morning"; 
+    
+    return (
+      <h2>{greeting}, Frank.</h2> // returns a React element
+   );
+}
+
+export default DisplayList;
